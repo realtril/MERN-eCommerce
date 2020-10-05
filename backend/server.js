198da31e8a33371ca222817804c05b652def8d81
Oleksandr Tril <tril.oleksandr@gmail.com>
2020-10-06 00:51:11 +0300

    added error handling

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import connectDB from "./config/db.js";
 import products from "./data/products.js";
 import colors from "colors";
 import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 const app = express();
 
@@ -16,6 +17,10 @@ app.get("/", (req, res, next) => {
 });
 
 app.use("/api/products", productRoutes);
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
