dc3e54e4afe8a4a0eedcd68e6495fabaa581de7d
Oleksandr Tril <tril.oleksandr@gmail.com>
2020-11-15 14:04:33 +0200

    user registration middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,16 @@
 import { Router } from "express";
 const router = Router();
-import { authUser, getUserProfile } from "../controllers/userControllers.js";
+import {
+  authUser,
+  registerUser,
+  getUserProfile,
+} from "../controllers/userControllers.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 //@desc     Fetch all products
 //@route    GET /api/products
 //@access   Public
+router.route("/").post(registerUser);
 router.post("/login", authUser);
 router.route("/profile").get(protect, getUserProfile);
 
