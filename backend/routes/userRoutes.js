9798063694126598c944a34d78cc7168f27aac5f
Oleksandr Tril <tril.oleksandr@gmail.com>
2020-11-15 13:03:31 +0200

    custom auth middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,12 @@
 import { Router } from "express";
 const router = Router();
-import { authUser } from "../controllers/userControllers.js";
+import { authUser, getUserProfile } from "../controllers/userControllers.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 //@desc     Fetch all products
 //@route    GET /api/products
 //@access   Public
 router.post("/login", authUser);
-//@desc     Fetch a single product
-//@route    GET /api/products/:id
-//@access   Public
+router.route("/profile").get(protect, getUserProfile);
 
 export default router;
