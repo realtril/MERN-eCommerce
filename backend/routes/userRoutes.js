21e8601cc4178ef4ffc7a8a4b262a1a956da1a71
Oleksandr Tril <tril.oleksandr@gmail.com>
2020-11-29 22:05:59 +0200

    checking the problem

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,6 +4,7 @@ import {
   authUser,
   registerUser,
   getUserProfile,
+  updateUserProfile,
 } from "../controllers/userControllers.js";
 import { protect } from "../middleware/authMiddleware.js";
 
@@ -12,6 +13,9 @@ import { protect } from "../middleware/authMiddleware.js";
 //@access   Public
 router.route("/").post(registerUser);
 router.post("/login", authUser);
-router.route("/profile").get(protect, getUserProfile);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 export default router;
