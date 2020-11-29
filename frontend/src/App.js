66e05b4d05dda9297fdeb92ba59e537471414b5c
Oleksandr Tril <tril.oleksandr@gmail.com>
2020-11-29 13:54:01 +0200

    fixed bug

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
-import RegisterScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
 
 const App = () => {
   return (
