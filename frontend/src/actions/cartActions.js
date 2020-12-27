ba24289132806797452f0fa544a818197f69bc5e
Oleksandr Tril <tril.oleksandr@gmail.com>
2020-12-27 21:57:48 +0200

    adding a placeorder screen

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -4,6 +4,7 @@ import {
   CART_ADD_ITEM,
   CART_REMOVE_ITEM,
   CART_SAVE_SHIPPING_ADDRESS,
+  CART_SAVE_PAYMENT_METHOD,
 } from '../constants/cartConstants';
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
@@ -32,10 +33,20 @@ export const removeFromCart = id => (dispatch, getState) => {
 };
 
 export const saveShippingAddress = data => dispatch => {
+  console.log(data);
   dispatch({
     type: CART_SAVE_SHIPPING_ADDRESS,
     payload: data,
   });
 
-  localStorage.setItem('shippingAddress', data);
+  localStorage.setItem('shippingAddress', JSON.stringify(data));
+};
+
+export const savePaymentMethod = data => dispatch => {
+  dispatch({
+    type: CART_SAVE_PAYMENT_METHOD,
+    payload: data,
+  });
+
+  localStorage.setItem('paymentMethod', JSON.stringify(data));
 };
